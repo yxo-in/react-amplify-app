7c1dcbf6b9fccd0c24ca1f2fae27a8ca93267250
yxo-in <itsvavalol@gmail.com>
2021-09-05 18:16:04 +0800

    lex 2

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hello from Amplify!!!! 16082021
-          update 1 : test lex : 05092021
+          Hello from Amplify!!!! 
+          
         </h1>
+        <h3>Activity log</h3>
+        
+        <a>Initial - 16082021 <br></br> 
+        update 1 : test lex : 05092021
+        </a>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,16 +25,16 @@ function App() {
           Learn React
         </a>
 
-        <script src="https://d2f33vy3qg049i.cloudfront.net/lex-web-ui-loader.min.js"></script>
-          <script>{`
-            var loaderOpts = {
-              baseUrl: 'https://d2f33vy3qg049i.cloudfront.net/',
-              shouldLoadMinDeps: true
-            };
-            var loader = new ChatBotUiLoader.IframeLoader(loaderOpts);
-            loader.load()
-              .catch(function (error) { console.error(error); });
-            `}</script>
+        <script src="https://d1olhb9k3nup61.cloudfront.net/lex-web-ui-loader.min.js"></script>
+        <script> {`
+          var loaderOpts = {
+            baseUrl: 'https://d1olhb9k3nup61.cloudfront.net/',
+            shouldLoadMinDeps: true
+          };
+          var loader = new ChatBotUiLoader.IframeLoader(loaderOpts);
+          loader.load()
+            .catch(function (error) { console.error(error); });
+        `}</script>
 
       </header>
     </div>
