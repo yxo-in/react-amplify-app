c0fd2171dd4365be8fdbba992e4edf9f085a4651
yxo-in <itsvavalol@gmail.com>
2021-09-08 17:21:09 +0800

    lex - separate js 2

--- src/lex.js
+++ src/lex.js
@@ -2,6 +2,6 @@ var loaderOpts = {
   baseUrl: 'https://d1olhb9k3nup61.cloudfront.net/',
   shouldLoadMinDeps: true
 };
+
 var loader = new ChatBotUiLoader.IframeLoader(loaderOpts);
-loader.load()
-  .catch(function (error) { console.error(error); });
+loader.load();
