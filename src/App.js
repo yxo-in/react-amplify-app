3ce8e0dfaf60299d634c41ce9c271afb9ee882e1
yxo-in <itsvavalol@gmail.com>
2021-09-08 19:52:39 +0800

    lex - separate js 6

--- src/App.js
+++ src/App.js
@@ -25,19 +25,21 @@ function App() {
         >
           Learn React
         </a>
-        <Helmet>
-        <script src="https://d1olhb9k3nup61.cloudfront.net/lex-web-ui-loader.min.js"></script>
-          <script> 
-            var loaderOpts = {
-              `baseUrl: 'https://d1olhb9k3nup61.cloudfront.net/',
-              shouldLoadMinDeps: true`
-            };
-            var loader = new ChatBotUiLoader.IframeLoader(loaderOpts);
-            loader.load()
-              .catch(function (error) { console.error(error); });
-          </script>
+        <div>
+          <Helmet>
+            <script src="https://d1olhb9k3nup61.cloudfront.net/lex-web-ui-loader.min.js"></script>
+              <script> 
+                var loaderOpts = {
+                  `baseUrl: 'https://d1olhb9k3nup61.cloudfront.net/',
+                  shouldLoadMinDeps: true`
+                };
+                var loader = new ChatBotUiLoader.IframeLoader(loaderOpts);
+                loader.load()
+                  .catch(function (error) { console.error(error) });
+              </script>
   
-        </Helmet>
+          </Helmet>
+        </div>
 
       </header>
     </div>
